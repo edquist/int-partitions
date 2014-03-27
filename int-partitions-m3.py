936b9352a1e97136d7b9e514f86e67984284589f
Carl Edquist <edquist@cs.wisc.edu>
2014-03-27 15:57:23 -0500

    woo!

--- int-partitions-m3.py
+++ int-partitions-m3.py
@@ -3,12 +3,12 @@
 import sys
 import re
 import math
-#from math import ceil
-#from math import floor
-import math
 
-def iceil(x):  return int(math.ceil(x))
+# convenience functions
+def  iceil(x): return int(math.ceil(x))
 def ifloor(x): return int(math.floor(x))
+def iround(x): return int(round(x))
+def zsort(*x): return zip(*sorted(zip(*x)))
 
 ints = [int(line) for line in sys.stdin if re.search(r'^\s*\d+\s*$', line)]
 
@@ -18,19 +18,25 @@ w = max(len(str(i)) for i in ints)
 
 P_S = float(P)/S
 qq = [n * P_S for n in ints]
-rr = map(int,map(round,qq))
+rr = map(iround,qq)
+ii = range(len(qq))
+ee = [rr[i] - qq[i] for i in ii]
 R = sum(rr)
-U = sum(map(int,map(ceil,qq)))
-V = sum(map(int,map(floor,qq)))
+U = sum(map(iceil,qq))
+V = sum(map(ifloor,qq))
 
-ee = [rr[i] - qq[i] for i in range(len(qq))]
+_,_,ii2 = zsort(ee,qq,ii)
+_,mm = zsort(ii2,ii)
 
-qq2,ii2=zip(*sorted(zip(qq,range(len(qq)))))
-mm = list(enumerate(ii2))
+pp = [ iceil(qq[i]) if mm[i] <  P-R      else
+      ifloor(qq[i]) if mm[i] >= P-R + n  else rr[i] for i in ii ]
 
-for n in ints:
-    t += n
-    slot = P*t/s - P*(t-n)/s
+#import __main__
+#print "---"
+#for v in "R P U V".split():
+#    print "%s: %d" % (v,__main__.__dict__[v])
+#print "---"
 
+for n,slot in zip(ints,pp):
     print "%*d: %d%%" % (w, n, slot)
 
